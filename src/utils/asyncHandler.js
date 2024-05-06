1903d4dded1b86d6080db7e1be3775f4128631b1
Parveen Kashyap <xmen.haryana@gmail.com>
2024-05-06 12:32:21 +0530

    asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,41 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve( requestHandler(req,res,next))
+        .catch((error)=>{
+            console.log("Error in async handler fn !!",error);
+            next(error);
+        })
+    }
+}
+
+
+export {asyncHandler};
+
+
+
+
+
+
+
+/*
+all version
+const asyncHandler = ()=>{}
+const asyncHandler = (fn)=>{async()=>{}}
+const asyncHandler = (fn)=> async () => {}
+
+const asyncHandler = (fn) => async (req,res,next) =>{
+    try {
+         await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code || 400).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+}
+
+
+
+
+*/
