e0c8f9cfb900421d0002321b2cea4c7bbfb063cf
Parveen Kashyap <xmen.haryana@gmail.com>
2024-05-06 11:56:20 +0530

    cookie

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import connectDB from "./db/index.js"
 import { app } from './app.js';
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './.env'
+})
+
 
 const port = process.env.PORT || 4000;
 
