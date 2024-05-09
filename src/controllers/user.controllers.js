52adf835d4825469bc99e47f016f22fc9197f235
Parveen Kashyap <xmen.haryana@gmail.com>
2024-05-09 16:55:08 +0530

    register user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,8 +17,8 @@ const registerUser = asyncHandler( async (req,res)=>{
     // remove password and refresh token field from response
     // check for user creation
     // return res
-    
-const {username, email, fullName, password} = req.body
+
+const {username, email, fullName, password} = req.body    
     if (
         [username, email, fullName, password].some((field) => field?.trim() === "")
     ) {
@@ -28,12 +28,15 @@ const {username, email, fullName, password} = req.body
     const existedUser = await User.findOne({
         $or:[{ username },{ email }]
     });
-    if (!existedUser) {
+
+    if ( existedUser ) {
         throw new ApiError(409, "User with email or username already exists")
     }
 
     //console.log(req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
@@ -46,6 +49,10 @@ const {username, email, fullName, password} = req.body
     }
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
     const user = await User.create({
         username:username.toLowerCase(),
         email,
@@ -54,17 +61,23 @@ const {username, email, fullName, password} = req.body
         avatar:avatar.url,
         coverImage:coverImage?.url || ""
     });
+
     const createdUser = await User.findById(user._id).select("-password -refreshToken");
     
-    if (!createdUser) {
+    if ( !createdUser ) {
         throw new ApiError(500, "Something went wrong while registering the user")
     };
 
-
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User Register Successfully")
+    )
     
 })
 
-export {registerUser};
+export {
+    registerUser,
+};
+
 /*
 const registerUser = asyncHandler (async(req,res)=>{
     res.send("Hello World")
