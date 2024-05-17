02cda12e741661b21bd7fd90bca40f80a8a000b1
Parveen Kashyap <xmen.haryana@gmail.com>
2024-05-17 15:55:23 +0530

    finally banckend series finished

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const options = {
   httpOnly: true,
@@ -382,20 +383,75 @@ const getUserChannerProfile = asyncHandler ( async (req, res) => {
     return res
     .status(200)
     .json(
-      new ApiResponse(200, channel[0], "Channel found successfully")
+      new ApiResponse(200, channel[0], "User Channel found successfully")
     )
-})
+});
+
+const getWatchHistory = asyncHandler ( async ( req, res ) => {
+   const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+       // _id: new mongoose.Types.ObjectId.createFromHexString(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+   ]);
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200,
+      user[0].watchHistory,
+      "Watch History fetched successfully")
+   )
+});
 
 
 
 export {
-registerUser,
-loginUser,
-logoutUser,
-refreshToken,
-changeCurrentPassword,
-getCurrentUser,
-updateAccountDetails,
-updateUserAvatar,
-updateUserCoverImage
-};
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannerProfile,
+    getWatchHistory
+  };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshToken, changeCurrentPassword} from "../controllers/user.controllers.js"
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannerProfile,
+    getWatchHistory
+  } from "../controllers/user.controllers.js"
 import {upload} from "../middleware/multer.middleware.js"
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -23,7 +35,13 @@ router.route("/login").post(loginUser);
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshToken);
-router.route("/password").post(verifyJWT,changeCurrentPassword);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/update-coverimage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT,getUserChannerProfile);
+router.route("/user-watchhistory").patch(verifyJWT,getWatchHistory);
 
 
 
